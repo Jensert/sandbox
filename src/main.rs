9abcd70e61cfaa7f69cf4da5bcae9f85fa59f878
jensert <jensert@local.com>
2025-09-30 18:29:54 +0200

    Added basic UI

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,8 @@
-use macroquad::{main, prelude::*};
+use macroquad::{
+    main,
+    prelude::*,
+    ui::{self, hash, root_ui, widgets},
+};
 mod app;
 mod pixel;
 mod pixel_grid;
@@ -6,16 +10,38 @@ use app::{App, window_settings};
 
 #[main(window_settings)]
 async fn main() {
-    let mut app = App::new((160, 90));
+    let mut app = App::new((320, 180));
     while app.running() {
         app.handle_input();
         app.start_drawing();
         clear_background(SKYBLUE);
 
         app.pixels().draw();
-        app.pixels_mut().update();
+
+        widgets::Window::new(hash!(), vec2(0.0, 0.0), vec2(200.0, 200.0))
+            .label("Info")
+            .movable(true)
+            .titlebar(true)
+            .ui(&mut *root_ui(), |ui| {
+                ui.label(None, format!("FPS: {}", get_fps()).as_str());
+                ui.label(
+                    None,
+                    format!("# Pixels: {}", app.pixels().grid().len()).as_str(),
+                );
+                ui.separator();
+                if ui.button(None, "Clear pixels") {
+                    app.pixels_mut().grid_mut().clear();
+                }
+                ui.label(
+                    None,
+                    format!("Selected pixel: {}", app.selected_pixel().get()).as_str(),
+                );
+            });
 
         app.stop_drawing();
+
+        app.pixels_mut().update();
+
         next_frame().await;
     }
 }
