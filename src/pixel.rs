32aaf82f4251d3412bf07034ff6dad6fd5393c24
jensert <j.greeuw@hotmail.com>
2025-09-30 17:38:09 +0200

    Restructured some code Pixel grid updates now rely on GridMovement struct These are all used in the updating functions new updating behaviour can easily be made by adding new functions that take a GridMovement and modify that based on certain conditions
    Also added some randomization to the physics
    and updating of pixels

--- src/pixel.rs
+++ src/pixel.rs
@@ -1,16 +1,153 @@
-use crate::pixel_grid::PixelGrid;
-use macroquad::prelude::*;
+use crate::pixel_grid::{GridMovement, PixelGrid};
+use macroquad::{prelude::*, rand::RandGenerator};
 
-#[derive(Clone, Copy)]
+#[derive(PartialEq, Clone, Copy, Debug)]
 pub enum PixelType {
     Sand,
     Water,
 }
+impl PixelType {
+    pub fn next(&mut self) {
+        match *self {
+            PixelType::Sand => *self = PixelType::Water,
+            PixelType::Water => *self = PixelType::Sand,
+        }
+    }
+    pub fn previous(&mut self) {
+        match *self {
+            PixelType::Sand => *self = PixelType::Water,
+            PixelType::Water => *self = PixelType::Sand,
+        }
+    }
 
-pub trait PixelBehaviour {
-    fn update(&self, pixelgrid: &PixelGrid, x: u32, y: u32) -> Option<((u32, u32), (u32, u32))>;
+    pub fn update(
+        &self,
+        pixel_grid: &PixelGrid,
+        x: u32,
+        y: u32,
+        rng: &RandGenerator,
+    ) -> Option<GridMovement> {
+        match self {
+            PixelType::Sand => update_sand(pixel_grid, x, y, rng),
+            PixelType::Water => update_water(pixel_grid, x, y, rng),
+        }
+    }
 
-    fn draw(&self, x: u32, y: u32);
+    /// Returns a boolean indicating whether the pixel was correctly updated or not
+    /// It was updated if the space below the current position was free
+    /// if it was updated you should probably skip any oother updates in that same frame
+    pub fn apply_gravity(&self, pixel_grid: &PixelGrid, grid_movement: &mut GridMovement) -> bool {
+        let check_position = (
+            grid_movement.old_position.0,
+            grid_movement.old_position.1 + 1,
+        );
+        if pixel_grid.get(check_position).is_free() {
+            grid_movement.new_position.1 = check_position.1;
+            return true;
+        }
+        false
+    }
+
+    pub fn fall(
+        &self,
+        pixel_grid: &PixelGrid,
+        grid_movement: &mut GridMovement,
+        direction: i8,
+    ) -> bool {
+        if direction == 0 {
+            // If there is a sand pixel below, check 1 down and right
+            let check_position = (
+                grid_movement.old_position.0 + 1,
+                grid_movement.old_position.1 + 1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+
+            // If there is a sand pixel below, check 1 down and left
+            let check_position = (
+                grid_movement.old_position.0 - 1,
+                grid_movement.old_position.1 + 1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+        } else {
+            // If there is a sand pixel below, check 1 down and left
+            let check_position = (
+                grid_movement.old_position.0 - 1,
+                grid_movement.old_position.1 + 1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+
+            // If there is a sand pixel below, check 1 down and right
+            let check_position = (
+                grid_movement.old_position.0 + 1,
+                grid_movement.old_position.1 + 1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    pub fn settle(
+        &self,
+        pixel_grid: &PixelGrid,
+        grid_movement: &mut GridMovement,
+        direction: i8,
+    ) -> bool {
+        // Check random direction to see if it should first move left or right
+        if direction == 0 {
+            // If there is a water pixel below, check 1 down and right
+            let check_position = (
+                grid_movement.old_position.0 + 1,
+                grid_movement.old_position.1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+
+            // If there is a water pixel below, check 1 down and left
+            let check_position = (
+                grid_movement.old_position.0 - 1,
+                grid_movement.old_position.1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+        } else {
+            // If there is a water pixel below, check 1 down and left
+            let check_position = (
+                grid_movement.old_position.0 - 1,
+                grid_movement.old_position.1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+
+            // If there is a water pixel below, check 1 down and right
+            let check_position = (
+                grid_movement.old_position.0 + 1,
+                grid_movement.old_position.1,
+            );
+            if pixel_grid.get(check_position).is_free() {
+                grid_movement.new_position = check_position;
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 pub fn draw_pixel(pixel_type: PixelType, x: u32, y: u32) {
@@ -25,67 +162,73 @@ pub fn draw_pixel(pixel_type: PixelType, x: u32, y: u32) {
 }
 
 pub fn update_sand(
-    pixelgrid: &PixelGrid,
+    pixel_grid: &PixelGrid,
     x: u32,
     y: u32,
-) -> Option<((u32, u32), (u32, u32), PixelType)> {
-    let mut res = None; // Res will be returned. contains current and new position and the pixel
-    // Check if y position is bottom of the screen
-    if y < pixelgrid.height() - 1 {
-        // Check if there is no sand pixel below current position
-        let mut new_pos = (x, y + 1); // If it isn't set new pos 1 pixel down
-        let pixel_type = PixelType::Sand;
-        if pixelgrid.get(new_pos).is_none() {
-            res = Some(((x, y), new_pos, pixel_type)); // return the old pos, new pos and the pixel data
-            return res;
-        }
+    rng: &RandGenerator,
+) -> Option<GridMovement> {
+    let old_position = (x, y);
+    let mut new_position = old_position; // new position to be returned
+    let pixel_type = PixelType::Sand;
 
-        // If there is a sand pixel below, check 1 down and right
-        new_pos.0 += 1;
-        if pixelgrid.get(new_pos).is_none() {
-            res = Some(((x, y), new_pos, pixel_type)); // return the old pos, new pos and the pixel data
-            return res;
-        }
+    let mut grid_movement = GridMovement::new(old_position, new_position, pixel_type);
 
-        // If there is a sand pixel below, check 1 down and left
-        new_pos.0 -= 2;
-        if pixelgrid.get(new_pos).is_none() {
-            res = Some(((x, y), new_pos, pixel_type)); // return the old pos, new pos and the pixel data
-            return res;
-        }
+    if pixel_type.apply_gravity(pixel_grid, &mut grid_movement) {
+        return Some(grid_movement);
     }
-    return res;
+
+    let direction = rng.gen_range(0, 2);
+    if pixel_type.fall(pixel_grid, &mut grid_movement, direction) {
+        return Some(grid_movement);
+    }
+    return None;
 }
 
 pub fn update_water(
-    pixelgrid: &PixelGrid,
+    pixel_grid: &PixelGrid,
     x: u32,
     y: u32,
-) -> Option<((u32, u32), (u32, u32), PixelType)> {
-    let mut res = None; // Res will be returned. contains current and new position and the pixel
+    rng: &RandGenerator,
+) -> Option<GridMovement> {
     // Check if y position is bottom of the screen
-    if y < pixelgrid.height() - 1 {
-        // Check if there is no sand pixel below current position
-        let mut new_pos = (x, y + 1); // If it isn't set new pos 1 pixel down
-        let pixel_type = PixelType::Water;
-        if pixelgrid.get(new_pos).is_none() {
-            res = Some(((x, y), new_pos, pixel_type)); // return the old pos, new pos and the pixel data
-            return res;
-        }
-
-        // If there is a sand pixel below, check 1 down and right
-        new_pos.0 += 1;
-        if pixelgrid.get(new_pos).is_none() {
-            res = Some(((x, y), new_pos, pixel_type)); // return the old pos, new pos and the pixel data
-            return res;
-        }
-
-        // If there is a sand pixel below, check 1 down and left
-        new_pos.0 -= 2;
-        if pixelgrid.get(new_pos).is_none() {
-            res = Some(((x, y), new_pos, pixel_type)); // return the old pos, new pos and the pixel data
-            return res;
-        }
+    // Check if there is no water pixel below current position
+    let old_position = (x, y);
+    let mut new_position = (x, y + 1); // If it isn't set new pos 1 pixel down
+    let pixel_type = PixelType::Water;
+    if pixel_grid.get(new_position).is_free() {
+        let res = GridMovement::new(old_position, new_position, pixel_type); // return the old pos, new pos and the pixel data
+        return Some(res);
     }
-    return res;
+
+    // If there is a water pixel below, check 1 down and right
+    new_position = (x + 1, y + 1);
+    if pixel_grid.get(new_position).is_free() {
+        let res = GridMovement::new(old_position, new_position, pixel_type); // return the old pos, new pos and the pixel data
+        return Some(res);
+    }
+
+    // If there is a water pixel below, check 1 down and left
+    new_position = (x - 1, y + 1);
+    if pixel_grid.get(new_position).is_free() {
+        let res = GridMovement::new(old_position, new_position, pixel_type); // return the old pos, new pos and the pixel data
+        return Some(res);
+    }
+
+    // Finally check directly left and right
+
+    // If there is a water pixel below, check 1 down and right
+    new_position = (x + 1, y);
+    if pixel_grid.get(new_position).is_free() {
+        let res = GridMovement::new(old_position, new_position, pixel_type); // return the old pos, new pos and the pixel data
+        return Some(res);
+    }
+
+    // If there is a water pixel below, check 1 down and left
+    new_position = (x - 1, y);
+    if pixel_grid.get(new_position).is_free() {
+        let res = GridMovement::new(old_position, new_position, pixel_type); // return the old pos, new pos and the pixel data
+        return Some(res);
+    }
+
+    return None;
 }
